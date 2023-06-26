181677ce8109cf81cbdff0e7c0e84995e4867ae8
Rotee20Baht <dodohothot@hotmail.co.th>
2023-06-27 00:06:21 +0700

    fixed nav links

---
+++ app/place/page.jsx
@@ -0,0 +1,13 @@
+'use client'
+
+import Container from "../components/Container"
+
+export default function Place() {
+  return (
+    <section className="py-20">
+      <Container>
+        <h1>Places</h1>
+      </Container>
+    </section>
+  )
+}
