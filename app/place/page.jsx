979ac8028293805b6e571848953428b1e3977b8f
Rotee20Baht <Dodohothot@hotmail.com>
2023-07-18 21:44:12 +0700

    Added search place function on all places page

--- app/place/page.jsx
+++ app/place/page.jsx
@@ -1,46 +1,90 @@
+"use client";
+
+
+import { useState } from "react";
+import { useEffect } from "react";
+
+import axios from "axios";
+import { provinces } from "@/app/providers/SelectDataProvider";
+import { placttype } from "@/app/providers/SelectDataProvider";
+
 import Container from "../components/Container";
 import Selectbar from "../components/select/Selectbar";
 import Card from "../components/Card";
 import Link from "next/link";
-
-import { placttype } from "@/app/providers/SelectDataProvider";
+import SelectItem from "../components/select/SelectItem";
+import Button from "../components/Button";
 
 
 export default function Places() {
+  const [dataState, setDataState] = useState('กำลังโหลดข้อมูลสถานที่');
+  const [places, setPlaces] = useState([]);
+  const [province, setProvince] = useState();
+  const [types, setTypes] = useState();
 
-  const places = [
-    {
-      title: "วัดพระมหาธาตุวรมหาวิหาร",
-      provice: "นครศรีธรรมราช",
-      img: "https://youimg1.tripcdn.com/target/0ww5o12000acgzwvi54CC_C_880_350_R5.jpg",
-      rating: "5"
-    },
-    {
-      title: "หาดขนอม",
-      provice: "นครศรีธรรมราช",
-      img: "https://youimg1.tripcdn.com/target/10090s000000hlt6kB60F_C_880_350_R5.jpg?proc=source%2ftrip",
-      rating: "4.9"
-    },
-    {
-      title: "หอนาฬิกาตรัง",
-      provice: "ตรัง",
-      img: "https://youimg1.tripcdn.com/target/0ww6z120008zanvdt0EA9_C_760_506_R5.jpg?proc=source/trip",
-      rating: "5"
-    },
-    {
-      title: "ชินตาการ์เด้น",
-      provice: "ตรัง",
-      img: "https://youimg1.tripcdn.com/target/0ww5e120006oej226D8DB_C_760_506_R5.jpg?proc=source/trip",
-      rating: "4.9"
-    },
-  ]
+  useEffect(() => {
+    axios.get(`http://localhost:3000/api/place`)
+    .then((data) => {
+      console.log(data.data)
+      setPlaces(data.data);
+    })
+    .catch((err) => {
+      setDataState('ไม่พบข้อมูลสถานที่')
+    })
+  }, []);
+
+  const onProvinceChange = (value) => {
+    setProvince(value)
+  }
+
+  const onTypesChange = (value) => {
+    setTypes(value)
+  }
+
+  const onSubmit = () => {
+    let searhUrl = `http://localhost:3000/api/place?`
+
+    console.log({province, types})
+
+    if(province)
+      searhUrl+= `&province=${province}` 
+    if(types)
+      searhUrl+= `&types=${types}` 
+
+    console.log(searhUrl)
+
+    axios.get(searhUrl)
+    .then((data) => {
+      console.log(data.data)
+      setPlaces(data.data);
+    })
+    .catch((err) => {
+      setPlaces([]);
+      setDataState('ไม่พบข้อมูลสถานที่')
+    })
+  }
 
   return (
     <div className="pt-20 pb-4">
       <Container>
         <div className="w-full h-auto flex flex-col gap-4">
           <h1 className="font-semibold text-xl">ค้นหาสถานที่ทั้งหมด</h1>
-          <Selectbar title1="จังหวัด" title2="ประเภทสถานที่" title3="วันเปิดทำการ" options2={placttype}/>
+          <div className="w-full border shadow-sm rounded-xl p-4 bg-white">
+            <div className="w-full flex flex-col lg:flex-row gap-4">
+              <div className="w-full flex flex-col flex-1">
+                <SelectItem label="จังหวัด" options={provinces} onChange={(value) => onProvinceChange(value?.value)} />
+              </div>
+              <div className="w-full flex flex-col flex-1">
+                <SelectItem label="ประเภทสถานที่" options={placttype} onChange={(value) => onTypesChange(value?.value)}/>
+              </div>
+              <div className="w-full flex flex-col flex-1">
+                <SelectItem label="วันเวลาเปิด-ปิด" />
+              </div>
+              <div className="w-full flex flex-col flex-1">
+                <Button label="ค้นหา" onClick={onSubmit} />
+              </div>
+            </div>
+          </div>
           <div 
             className="
               w-full 
@@ -54,18 +98,23 @@ export default function Places() {
               sm:grid-cols-2
               md:grid-cols-3
               lg:grid-cols-4
-              gap-4"
+              gap-4
+              relative
+            "
             >
-              {places.map((item) => (
-                
-                <Card
-                  key={item.title}
-                  title={item.title}
-                  province={item.provice}
-                  img={item.img}
-                  rating={item.rating}
-                />
-                
+              {places.length <= 0 && (
+                <div className="absolute left-1/2 -translate-x-1/2 mt-1">{dataState}</div>
+              )}
+              {places && places.map((item) => (
+                <Link href={`/place/${item._id}`}>
+                  <Card
+                    key={item.name}
+                    title={item.name}
+                    province={item.province}
+                    img={item.images[0]}
+                    rating={item.rating}
+                  />
+                </Link>
               ))}
           </div>
         </div>  
